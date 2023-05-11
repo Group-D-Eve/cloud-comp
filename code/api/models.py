f8c8744e8db805386dffffffdf180228e4ae6945
Ssekyanzi-Joel <105359998+Ssekyanzi-Joel@users.noreply.github.com>
2023-05-11 15:30:18 +0300

    Removing errors

--- code/api/models.py
+++ code/api/models.py
@@ -8,32 +8,35 @@ class UnitMeasure(models.Model):
     Unit of measurements
     """
     id = models.AutoField(primary_key=True)
-    unit = models.CharField(max_length=50 ,null=False)
+    unit = models.CharField(max_length=50, null=False)
 
     def __str__(self) -> str:
         return self.unit
+
 
 class Status(models.Model):
     """
     status codes
     """
     id = models.AutoField(primary_key=True)
-    status = models.CharField(max_length=50 ,null=False)
+    status = models.CharField(max_length=50, null=False)
 
     def __str__(self) -> str:
         return self.status
+
 
 class People(models.Model):
     """
     people names and contact
     """
-    id = models.AutoField(primary_key= True)
+    id = models.AutoField(primary_key=True)
     firstName = models.CharField(max_length=150)
     lastName = models.CharField(max_length=150)
     email = models.EmailField(max_length=150)
 
     def __str__(self) -> str:
         return f"{self.firstName} {self.lastName}"
+
 
 class Attachment(models.Model):
     """
@@ -53,12 +56,12 @@ class PartCategory(models.Model):
     """
     part category for parts
     """
-    id=models.AutoField(primary_key=True)
-    category = models.CharField(max_length=150 ,verbose_name="part category", null=False)
+    id = models.AutoField(primary_key=True)
+    category = models.CharField(
+        max_length=150, verbose_name="part category", null=False)
 
     def __str__(self) -> str:
         return self.category
-
 
 
 class Supplier(models.Model):
@@ -66,32 +69,40 @@ class Supplier(models.Model):
     Supplier information
     """
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=150 ,null=False)
-    poc = models.ForeignKey(People, on_delete= models.deletion.CASCADE, related_name='supplierPoc', null=True, unique=False)
-    address = models.CharField(max_length=300, help_text="Enter full address with country", null=True)
+    name = models.CharField(max_length=150, null=False)
+    poc = models.ForeignKey(People, on_delete=models.deletion.CASCADE,
+                            related_name='supplierPoc', null=True, unique=False)
+    address = models.CharField(
+        max_length=300, help_text="Enter full address with country", null=True)
 
     def __str__(self) -> str:
         return self.name
+
 
 class Part(models.Model):
     """
     Part details
     """
-    id= models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=150, null=False)
-    category = models.ForeignKey(PartCategory, on_delete=models.deletion.CASCADE, related_name='partCategoryFk')
-    supplier = models.ForeignKey(Supplier, on_delete=models.deletion.CASCADE, related_name='supplierFk', null=True)
-    unit = models.ForeignKey(UnitMeasure, on_delete=models.deletion.CASCADE, related_name='partUnitFk', default=4)
+    category = models.ForeignKey(
+        PartCategory, on_delete=models.deletion.CASCADE, related_name='partCategoryFk')
+    supplier = models.ForeignKey(
+        Supplier, on_delete=models.deletion.CASCADE, related_name='supplierFk', null=True)
+    unit = models.ForeignKey(
+        UnitMeasure, on_delete=models.deletion.CASCADE, related_name='partUnitFk', default=4)
 
     def __str__(self) -> str:
         return self.name
+
 
 class MinimumStock(models.Model):
     """
     Minimum stocking level for inventory
     """
     id = models.AutoField(primary_key=True)
-    part = models.ForeignKey(Part, on_delete=models.deletion.CASCADE, related_name= "minStockPart")
+    part = models.ForeignKey(
+        Part, on_delete=models.deletion.CASCADE, related_name="minStockPart")
     minimumStock = models.PositiveIntegerField(null=False)
     lastUpdateDate = models.DateTimeField(auto_now=True)
 
@@ -101,9 +112,11 @@ class CurrentStock(models.Model):
     Current stock levels held in inventory
     """
     id = models.AutoField(primary_key=True)
-    part = models.ForeignKey(Part, on_delete=models.deletion.CASCADE, related_name='CurrentStockPart')
+    part = models.ForeignKey(
+        Part, on_delete=models.deletion.CASCADE, related_name='CurrentStockPart')
     currentStock = models.PositiveIntegerField(null=False)
     lastUpdateDate = models.DateTimeField(auto_now=True)
+
 
 class Order(models.Model):
     """
@@ -111,28 +124,33 @@ class Order(models.Model):
     """
     id = models.AutoField(primary_key=True)
     poNumber = models.PositiveIntegerField(null=False)
-    part = models.ForeignKey(Part, on_delete=models.deletion.CASCADE, related_name='orderPart')
+    part = models.ForeignKey(
+        Part, on_delete=models.deletion.CASCADE, related_name='orderPart')
     quantity = models.PositiveIntegerField(null=False)
     received = models.PositiveIntegerField(default=0)
-    unit = models.ForeignKey(UnitMeasure, on_delete=models.deletion.CASCADE, related_name='unitFk')
+    unit = models.ForeignKey(
+        UnitMeasure, on_delete=models.deletion.CASCADE, related_name='unitFk')
     dateOrdered = models.DateField(null=False)
     eta = models.DateField(null=False)
     dateDelivered = models.DateField(null=True)
-    status = models.ForeignKey(Status ,default=False, help_text="order status, true-> delivered, false-> incomplete", on_delete=models.deletion.CASCADE, related_name='orderStatus')
+    status = models.ForeignKey(Status, default=False, help_text="order status, true-> delivered, false-> incomplete",
+                               on_delete=models.deletion.CASCADE, related_name='orderStatus')
 
     def __str__(self) -> str:
         return f"po#:{self.poNumber}/part:{self.part}/ordered:{self.quantity}"
 
     class Meta:
-        ordering=['id',]
+        ordering = ['id',]
 
 
 class Receiving(models.Model):
     """
     for receieving attachements like packing slips, invoice etc
     """
-    id= models.AutoField(primary_key=True)
-    orderItem = models.ForeignKey(Order, on_delete=models.deletion.CASCADE, related_name='receivingOrder')
+    id = models.AutoField(primary_key=True)
+    orderItem = models.ForeignKey(
+        Order, on_delete=models.deletion.CASCADE, related_name='receivingOrder')
     quantity = models.PositiveIntegerField(null=False)
-    attachment = models.ForeignKey(Attachment, on_delete=models.deletion.CASCADE, related_name='receivingAttachment', null=True)
-    date= models.DateField(auto_now=True)
+    attachment = models.ForeignKey(
+        Attachment, on_delete=models.deletion.CASCADE, related_name='receivingAttachment', null=True)
+    date = models.DateField(auto_now=True)
