1f309acf0e36c0bda5293038857b968141c4e6d3
Ssekyanzi-Joel <105359998+Ssekyanzi-Joel@users.noreply.github.com>
2023-05-11 15:30:36 +0300

    Removing errors

--- code/frontend/urls.py
+++ code/frontend/urls.py
@@ -16,15 +16,23 @@ urlpatterns = [
     path('min/update', views.MinStockUpdate.as_view(), name='minUpdate'),
     path('min/upload', views.MinimumStockUpload.as_view(), name='minUpload'),
     path('purchasing', views.Purchasing.as_view(), name='purchasingHome'),
-    path('purchasing/create', views.PurchasingCreate.as_view(), name='purchasingCreate'),
-    path('purchasing/update/<int:pk>', views.PurchaseUpdate.as_view(), name='purchasingUpdate'),
-    path('purchasing/close',views.ClosePurchaseOrder.as_view(), name='purchasingClose' ),
-    path('purchasing/open/<int:pk>',views.OpenPurchaseOrder.as_view(), name='purchasingOpen' ),
-    path('purchasing/delete', views.DeletePurchaseOrder.as_view(), name='purchaseDelete'),
+    path('purchasing/create', views.PurchasingCreate.as_view(),
+         name='purchasingCreate'),
+    path('purchasing/update/<int:pk>',
+         views.PurchaseUpdate.as_view(), name='purchasingUpdate'),
+    path('purchasing/close', views.ClosePurchaseOrder.as_view(),
+         name='purchasingClose'),
+    path('purchasing/open/<int:pk>',
+         views.OpenPurchaseOrder.as_view(), name='purchasingOpen'),
+    path('purchasing/delete', views.DeletePurchaseOrder.as_view(),
+         name='purchaseDelete'),
     path('purchasing/edit', views.EditPurchaseOrder.as_view(), name='purchaseEdit'),
 
-    path('workingonit', TemplateView.as_view(template_name='inprogress/underWork.html'), name='inprog')
+    path('workingonit', TemplateView.as_view(
+        template_name='inprogress/underWork.html'), name='inprog'),
 
-    # path('test',(TemplateView.as_view(template_name='registration/login_test.html')), name='test'),
-    # path('workingonit', TemplateView.as_view(template_name='inprogress/underWork_normal.html'), name='inprog')
+    path('test', (TemplateView.as_view(
+        template_name='registration/login_test.html')), name='test'),
+    path('workingonit', TemplateView.as_view(
+        template_name='inprogress/underWork_normal.html'), name='inprog')
 ]
